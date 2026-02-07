90a36618b66ddaf1fa5d88f5cea92786f595a723
KnockoffofKnockoff <156373475+KnockoffofKnockoff@users.noreply.github.com>
2026-02-07 11:48:11 -0500

    cleaned up code for intake project

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,12 +10,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-//added gb
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Timer;
-
 
 //import com.revrobotics.spark.*;
 import com.revrobotics.spark.SparkMax;
@@ -31,30 +27,20 @@ import com.revrobotics.spark.SparkBase.ResetMode;
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
+  // Initialize autonomous choices
   private static final String kDefaultAuto = "Default";
-  private static final String kCenterForward = "Center Move Forward";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  // Initialize controller
+  private final XboxController controller0 = new XboxController(0);
+  // Initialize motors
+  private final SparkMax leftMotor = new SparkMax(1, MotorType.kBrushless);
+  private final SparkMax rightMotor = new SparkMax(2, MotorType.kBrushless);
+  // Initialize motor config
+  private final SparkMaxConfig intakeConfig = new SparkMaxConfig();
 
-  //gb added
-    private final XboxController controller0 = new XboxController(0);
-    private final SparkMax leftMotor = new SparkMax(1, MotorType.kBrushless);
-    private final SparkMax rightMotor = new SparkMax(2, MotorType.kBrushless);
-
-    private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotor, rightMotor);
-    private final SparkMaxConfig driveConfig = new SparkMaxConfig();
-
-    private final Timer timer1 = new Timer();
-    //used in teleop to control drive speed... slow it down so not so jumpy
-    private double driveSpeed = 1;
-
-   
-
-
-
-    //private final DifferentialDrive robotDrive =
-    //        new DifferentialDrive(leftMotor::set, rightMotor::set);
+  private final Timer timer1 = new Timer();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -62,27 +48,18 @@ public class Robot extends TimedRobot {
    */
   public Robot() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("Center Move Forward", kCenterForward);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    //gb add        
-    SendableRegistry.addChild(robotDrive, leftMotor);
-    SendableRegistry.addChild(robotDrive, rightMotor);
-    // Configure Spark Max motor controllers 
-    driveConfig.smartCurrentLimit(60);
-    driveConfig.voltageCompensation(12.0);
-    
+    // Configure Spark Max motor controllers.
+    intakeConfig.smartCurrentLimit(60);
+    intakeConfig.voltageCompensation(12.0);
 
     // Apply configuration to left motor
-    leftMotor.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // Apply configuration to right motor and invert it
-    driveConfig.inverted(true);
-    rightMotor.configure(driveConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Disable the right motor, we don't need it right now
+    rightMotor.disable();
 
-    // Persist parameters to retain configuration in the event of a power cycle
-
-
-    //start timer
+    // Start timer
     timer1.start();
   
   }
@@ -110,65 +87,17 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-    //gb added reset timer1 to zero
-    timer1.restart();
-    //git demo comment
 
+    System.out.println("Auto selected: " + m_autoSelected);
+    timer1.restart();
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCenterForward:
-        // drive forward
-        //stop after 2 seconds
-        if(timer1.get() < 2) {
-          robotDrive.tankDrive(0.5,0.5); //drive forward at half speed
-        } 
-        else if(timer1.get() < 4) { //spin for 2 seconds
-          robotDrive.tankDrive(0.5,0); 
-        } else {  //stop
-          robotDrive.tankDrive(0.0,0.0); 
-        }
-
-
-        break;
       case kDefaultAuto:
       default:
-
-if(timer1.get() < 2) {
-          robotDrive.tankDrive(0.2,0.0); //drive forward at half speed
-        } 
-        else if(timer1.get() < 5) { //spin for 2 seconds
-          robotDrive.tankDrive(0.2,0); 
-        }
-        else if(timer1.get() < 7) {
-          robotDrive.tankDrive(0.5,0); 
-        }
-        else if(timer1.get() < 9) {
-          robotDrive.tankDrive(0.5,0);
-
-        } else {  //stop
-          robotDrive.tankDrive(0.0,0.0); 
-        }
-
-
-
-        // Put default auto code here
-        //GB added
-        /* better code below *
-        leftMotor.set(0.5); //half speed forward
-        rightMotor.set(0.5);
-        Timer.delay(2.0); //drive for 2 seconds
-        leftMotor.set(0); //stop
-        rightMotor.set(0);  
-        */
-
-        //more elegant solution
-        //robotDrive.tankDrive(0.5,0.5);
         break;
     }
   }
@@ -180,32 +109,12 @@ if(timer1.get() < 2) {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
+    if (controller0.getLeftBumperPressed()) {}
 
-         System.out.println("==========I am moving!==========");
-
-if (controller0.getLeftBumperPressed()) {
-  driveSpeed = 2; //divide by 2 to get 50% speed
-
-}
-if (controller0.getRightBumperPressed()) {
-  driveSpeed = driveSpeed - 0.1;  //increase speed by 10%
-  if (driveSpeed > 1) {
-    driveSpeed = 1;
-  }
-  System.out.println("Drive Speed Set to: " + driveSpeed);
-}   
-         // Drive with tank drive.
+    if (controller0.getRightBumperPressed()) {}   
        
-        // read controller joystick value range -1.0 to +1.0 note flip the sign as joystick values are reversed
-        // robotDrive.tankDrive(-controller0.getLeftY()/driveSpeed, -controller0.getRightY()/driveSpeed);
-
-        //arcade drive 
-        // That means that the Y axis drives forward the left stick
-        // and backward, and the X turns left and right the right stick
-        robotDrive.arcadeDrive(-controller0.getLeftY()/driveSpeed, -controller0.getRightX()/driveSpeed);
-
-
-
+    // read controller joystick value range -1.0 to +1.0 note flip the sign as joystick values are reversed
+    // robotDrive.tankDrive(-controller0.getLeftY()/driveSpeed, -controller0.getRightY()/driveSpeed);
   }
 
   /** This function is called once when the robot is disabled. */
